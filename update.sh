5358df6b0abe01d1ad75804ee91ae4258a72f16e
Lukas Kastern <lukaskastern@web.de>
2024-06-19 19:46:01 +0200

    Backport latest update.sh

--- update.sh
+++ update.sh
@@ -2,14 +2,36 @@
 set -euo pipefail
 set -x
 
-rm -rf GL/ GLES/ GLES2/ GLES3/ GLSC/ GLSC2/
-rm -rf _registry
-git clone https://github.com/KhronosGroup/OpenGL-Registry --depth 1 _registry
-mv _registry/api/GL .
-mv _registry/api/GLES .
-mv _registry/api/GLES2 .
-mv _registry/api/GLES3 .
-mv _registry/api/GLSC .
-mv _registry/api/GLSC2 .
-rm -rf _registry
+OPENGL_REV=22ffb40aef5203367ad9a1431cabbba30d0230b9
+EGL_REV=b055c9b483e70ecd57b3cf7204db21f5a06f9ffe
+MESA_REV=4baea976bb585013124b91dc4e9aef793f36605a
 
+# `git clone --depth 1` but at a specific revision
+git_clone_rev() {
+    repo=$1
+    rev=$2
+    dir=$3
+
+    rm -rf "$dir"
+    mkdir "$dir"
+    pushd "$dir"
+    git init -q
+    git fetch "$repo" "$rev" --depth 1
+    git checkout -q FETCH_HEAD
+    popd
+}
+
+rm -rf GL GLES GLES2 GLES3 GLSC2 EGL KHR
+
+git_clone_rev https://github.com/KhronosGroup/OpenGL-Registry.git "$OPENGL_REV" _opengl
+mv _opengl/api/{GL,GLES,GLES2,GLES3,GLSC2} .
+rm -rf GLES/1.0 # this directory contains headers for GLES 1.0 that we can't support
+rm -rf _opengl
+
+git_clone_rev https://github.com/KhronosGroup/EGL-Registry.git "$EGL_REV" _egl
+mv _egl/api/{EGL,KHR} .
+rm -rf _egl
+
+git_clone_rev https://gitlab.freedesktop.org/mesa/mesa.git "$MESA_REV" _mesa
+mv _mesa/include/GL/{gl,glx}.h GL
+rm -rf _mesa
